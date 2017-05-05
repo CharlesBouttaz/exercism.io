5b4dcdcaf10c267361cd8355e0dfad68495b4bbd
Charles Bouttaz <c.bouttaz@gmail.com>
2017-05-05 20:26:57 +0200

    rna-transcription without if :)

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -8,24 +8,16 @@ const complementDictionary = {
     'T': 'A',
 };
 
+//Maybe there is a better way...
+const error = () => {
+    throw new Error('Invalid input')
+};
+
 DnaTranscriber.prototype.toRna = function (sequence) {
     return sequence
         .split('')
-        .map(nucleotide => {
-            //Question : Can I get rid of this if statement ?
-            if (!complementDictionary[nucleotide]) {
-                throw new Error('Invalid input')
-            }
-            return complementDictionary[nucleotide]
-        })
+        .map(nucleotide => complementDictionary[nucleotide] || error.call())
         .join('');
 };
-
-// Option 1 : check the input and fail fast.
-// Disavantage : we loop two times over the array
-//sequence
-//    .split('')
-//    .filter(input => !Object.keys(complementDictionary).includes(input))
-//    .map(input => {throw new Error('Invalid input')});
 
 module.exports = DnaTranscriber;
